82b22361a1ca1304019a07458f945a61700e0483
Patrick Gibbs <patrickjkgibbs@gmail.com>
2024-09-16 15:16:07 -0700

    started to create submission form

--- app.js
+++ app.js
@@ -4,3 +4,15 @@ console.log("Let's get this party started!");
 
 const $form = $("#search-form");
 const $searchTerm = $("search-term");
+
+
+//submission of form
+
+$form.on("submit", async function(event) {
+    event.preventDefault(); //This will prevent the form submitting in a default manner
+
+    //getting search term from input field
+    const term = $searchTerm.val();
+
+    
+})
