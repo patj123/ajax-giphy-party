5692adccbc4895a4d071746b4bcfe0e21b382624
Patrick Gibbs <patrickjkgibbs@gmail.com>
2024-09-16 15:35:00 -0700

    debugged code

--- app.js
+++ app.js
@@ -1,48 +1,45 @@
 console.log("Let's get this party started!");
 
-//this seclects the form and input elements
-
+// Select the form and input elements
 const $form = $("#search-form");
-const $searchTerm = $("search-term");
+const $searchTerm = $("#search-term");
+const $gifContainer = $("#gif-container"); // Make sure this matches the ID in your HTML
 
+// Handle form submission
+$form.on("submit", async function (event) {
+    event.preventDefault(); // Prevent the form from submitting the default way
 
-//submission of form
-
-$form.on("submit", async function(event) {
-    event.preventDefault(); //This will prevent the form submitting in a default manner
-
-    //getting search term from input field
+    // Get the search term from the input field
     const term = $searchTerm.val();
 
     try {
+        // Make an AJAX request to the Giphy API using Axios
         const response = await axios.get("http://api.giphy.com/v1/gifs/search", {
             params: {
                 q: term,
                 api_key: "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym"
             }
         });
-        console.log(response.data) 
+
+        console.log(response.data);
+
+        // Check if there are any results
+        if (response.data.data.length > 0) {
+            // Get a random GIF from the results
+            const randomIdx = Math.floor(Math.random() * response.data.data.length);
+            const gifUrl = response.data.data[randomIdx].images.original.url;
+
+            // Create an img element and set its src attribute to the GIF URL
+            const $newGif = $("<img>", { src: gifUrl, class: "gif" });
+
+            // Append the new GIF to the gif-container
+            $gifContainer.append($newGif);
+        } else {
+            console.log("No GIFs were found for that search term.");
+        }
     } catch (error) {
         console.error("Error fetching data from Giphy API:", error);
     }
-
-    //are there any results?
-
-if (response.data.data.length > 0) {
-    //This gets a random gif from the results
-    const randomIdx = Math.floor(Math.ramdom() * response.data.data.length);
-    const gifUrl = response.data.data.length[randomIdx].images.original.url;
-
-    // Create img element and set src attribute the GIF URL
-    const $newGif = $("<img>", { src: gifUrl, class: "gif" });
-
-    //append gif to gif container
-
-    $gifContainer.append($newGif);
-} else {
-    console.log("No GIFs were found in that search term.")
-} catch (error) {
-    console.error("Error fetching data from Giphy API:", error);
-}
 });
+
 
